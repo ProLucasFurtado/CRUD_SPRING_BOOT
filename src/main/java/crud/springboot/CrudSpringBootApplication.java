50ca1d929ce91a4ad7a6d6a9e0e9af2320f6d4db
Lucas Furtado <lucasfurtado005@gmail.com>
2022-01-15 17:02:18 -0300

    É a classe principal junto com as anotações que rodam seu sistema.

--- src/main/java/crud/springboot/CrudSpringBootApplication.java
+++ src/main/java/crud/springboot/CrudSpringBootApplication.java
@@ -3,11 +3,11 @@ package crud.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Ler toda a nossa aplicação para colocar no ar
 public class CrudSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringBootApplication.class, args);
+		SpringApplication.run(CrudSpringBootApplication.class, args); //É a linha principal que roda o projeto Java Spring Boot
 	}
 
 }
