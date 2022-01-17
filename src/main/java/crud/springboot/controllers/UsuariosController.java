a732383c2592e9ed17de207d4ca79929a50e49d9
Lucas Furtado <lucasfurtado005@gmail.com>
2022-01-16 21:06:01 -0300

    Criado o Endpoint lista todos usuarios

--- src/main/java/crud/springboot/controllers/UsuariosController.java
+++ src/main/java/crud/springboot/controllers/UsuariosController.java
@@ -1,10 +1,15 @@
 package crud.springboot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +38,17 @@ public class UsuariosController {
 		usuario.setNome(nome);
 
 		usuarioRepository.save(usuario); //Grava no banco de dados
-		
+	
 		return "Olá Mundo " + nome;
+	}
+	
+	@GetMapping(value = "listatodos")//Método de API
+	@ResponseBody //Retorna os dados para o corpo da resposta
+	public ResponseEntity<List<Usuarios>> listaUsuarios() {
+		
+		List<Usuarios> usuarios = usuarioRepository.findAll(); //Executa a consulta no banco de dados
+		
+		return new ResponseEntity<List<Usuarios>>(usuarios, HttpStatus.OK); //Retorna a lista em JSON
 	}
 
 }
